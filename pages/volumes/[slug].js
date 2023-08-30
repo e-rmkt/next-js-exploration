bd26645f44228e5ebccf8ca6e61c4d841b483c4f
Eliza Romeikat <eliza-romeikat@gmx.de>
2023-08-30 17:17:41 +0200

    create first styled component

--- pages/volumes/[slug].js
+++ pages/volumes/[slug].js
@@ -2,42 +2,63 @@ import { volumes } from "@/lib/data";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import styled from "styled-components";
+import { GlobalStyle } from "@/styles";
+
+const StyledBox = styled.div`
+  background-color: ${({ color }) => color};
+`;
 
 export default function Page() {
   const router = useRouter();
 
   const slug = router.query.slug;
 
-  const volumeIndex = volumes.find((volume) => volume.slug === slug);
+  //   const volumeIndex = volumes.findIndex((volume) => volume.slug === slug);
 
+  //   const volume = volumes[volumeIndex];
   //   const nextVolume = volumes[volumeIndex + 1];
   //   const previousVolume = volumes[volumeIndex - 1];
 
-  const { title, description, cover, books } = volumeIndex;
+  //   const { title, description, cover, books, color } = volume;
+
+  const volumeIndex = volumes.findIndex((volume) => volume.slug === slug);
+
+  const volume = volumes[volumeIndex];
+  const nextVolume = volumes[volumeIndex + 1];
+  const previousVolume = volumes[volumeIndex - 1];
+
+  if (!volume) {
+    return null;
+  }
+
+  const { title, description, cover, books, color } = volume;
 
   return (
     <>
       <Link href="/volumes">← All Volumes</Link>
       <h1>{title}</h1>
       <p>{description}</p>
-      <ul>
-        {books.map((book) => {
-          return (
-            <li key={book.ordinal}>
-              {book.ordinal} <br />
-              {book.title}
-              <hr />
-            </li>
-          );
-        })}
-      </ul>
-      <Image
-        src={cover}
-        alt={`Cover image of ${title}`}
-        width={140}
-        height={230}
-      />
-      {/* {previousVolume ? (
+      <StyledBox color={color}>
+        <ul>
+          {books.map((book) => {
+            return (
+              <li key={book.ordinal}>
+                {book.ordinal} <br />
+                {book.title}
+                <hr />
+              </li>
+            );
+          })}
+        </ul>
+        <Image
+          src={cover}
+          alt={`Cover image of ${title}`}
+          width={140}
+          height={230}
+        />
+      </StyledBox>
+      {previousVolume ? (
         <div>
           <Link href={`/volumes/${previousVolume.slug}`}>
             ← Previous Volume: {previousVolume.title}
@@ -50,7 +71,8 @@ export default function Page() {
             Next Volume: {nextVolume.title} →
           </Link>
         </div>
-      ) : null} */}
+      ) : null}
+      <GlobalStyle />
     </>
   );
 }
